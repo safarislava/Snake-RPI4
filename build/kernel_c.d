f0c87b5eb796278869873942a4994ce28714d3a9
sadnex <s4dnex@gmail.com>
2025-06-19 16:14:00 +0300

    refactor: :fire: delete garbage

--- build/kernel_c.d
+++
@@ -1,2 +0,0 @@
-build/kernel_c.o: src/kernel.c include/common.h include/mini_uart.h \
- include/printf.h include/irq.h include/timer.h
--- build/timer_c.d
+++
@@ -1,3 +0,0 @@
-build/timer_c.o: src/timer.c include/printf.h include/peripherals/timer.h \
- include/peripherals/base.h include/common.h include/peripherals/irq.h \
- include/peripherals/_aux.h
